c0c4ca4a172cadc550218b76c416c2adfbea354b
Elsa-san <elsa.tartary@gmail.com>
2023-05-24 15:44:24 +0200

    EventListener added

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,24 @@
+const leftArrow = document.querySelector('.arrow_left')
+const rightArrow = document.querySelector('.arrow_right')
+
+leftArrow.addEventListener('click', function () { console.log("left") })
+rightArrow.addEventListener('click', function () { console.log("right") })
+
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
